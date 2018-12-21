4910aedc1e2202dd0e9098d9585d37d5a9c4f170
a0id <mattnappo@gmail.com>
2018-12-21 00:37:18 -0500

    framework done

--- switch.py
+++ switch.py
@@ -2,14 +2,19 @@ import json
 
 class Switch():
     def __init__(self, text):
-        self.text = text.lower()
+        self.text = self.process(text)
         self.phrases = { }
         self.load()
+    def process(self, text):
+        text = text.lower()
+        text = text.replace(",", "")
+        text = text.replace("'", "")
+        text = text.replace(".", "")
+        return text
     def load(self):
         with open("static/phrases.json", "r") as f:
             self.phrases = json.loads(f.read())
-        self.phrases = self.phrases["phrases"]
     def switch(self):
         for phrase in self.phrases:
-            self.text = self.text.replace(phrase.lower(), phrase[phrase])
+            self.text = self.text.replace(phrase.lower(), self.phrases[phrase])
         return self.text
